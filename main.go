69c11005ba22f169edf31d04d7a26e3c3dce47f4
Evans Ibok <evansib@live.com>
2017-07-21 11:24:12 +0100

    Declare variable with zero value

--- main.go
+++ main.go
@@ -3,13 +3,32 @@ package main
 import "fmt"
 
 func main() {
+
+	// Declare and initialize a variable
+
 	a := 20
 	b := "golang"
 	c := 3.59
 	d := true
 
+	// Print variable and typeof
+
 	fmt.Printf("%v %T \n", a, a)
 	fmt.Printf("%v %T \n", b, b)
 	fmt.Printf("%v %T \n", c, c)
 	fmt.Printf("%v %T \n", d, d)
+
+	// Declare variable with zero value
+
+	var e int
+	var f string
+	var g float64
+	var h bool
+
+	fmt.Printf("%v \n", e)
+	fmt.Printf("%v \n", f)
+	fmt.Printf("%v \n", g)
+	fmt.Printf("%v \n", h)
+
+	fmt.Println()
 }
